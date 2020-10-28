dea939f0aaec592b1b7b06d61b99946043236094
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2020-10-28 18:05:38 -0300

    Add simple wget commands for downloading the whole files

---
+++ utilities/download_datasets.sh
@@ -0,0 +1,3 @@
+# Download the zip files
+wget -O data/exp.zip http://www.mdic.gov.br/balanca/bd/comexstat-bd/ncm/EXP_COMPLETA.zip
+wget -O data/imp.zip http://www.mdic.gov.br/balanca/bd/comexstat-bd/ncm/IMP_COMPLETA.zip
