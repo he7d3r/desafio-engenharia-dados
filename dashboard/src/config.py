3eb5184e6d81038d6ca27e1bdaf997554c878127
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2020-11-28 11:32:46 -0300

    Remove useless object inheritance

--- dashboard/src/config.py
+++ dashboard/src/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-class Config(object):
+class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY')
