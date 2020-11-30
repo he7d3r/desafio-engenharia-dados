1b7e4383a3bc4ee9285cfc11aca5bb27250f3459
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2020-11-30 18:42:28 -0300

    Use a class for each function being tested, and a method for each test

--- dashboard/tests/test_app.py
+++ dashboard/tests/test_app.py
@@ -1,55 +1,54 @@
 import pytest
-from src.app import get_month_name, get_month_options, large_num_formatter
+from src.app import (get_month_name,
+                     large_num_formatter)
 
 
-def test_for_valid_month_values():
-    assert get_month_name(1) == 'janeiro'
-    assert get_month_name(12) == 'dezembro'
+class TestGetMonthName:
+
+    def test_for_valid_month_values(self):
+        assert get_month_name(1) == 'janeiro'
+        assert get_month_name(12) == 'dezembro'
+
+    def test_for_invalid_month_values(self):
+        for value in [0, 13, 'outro', '*']:
+            assert get_month_name(value) == 'todos os meses'
+
+    def test_for_return_type(self):
+        for month in range(1, 13):
+            assert isinstance(get_month_name(month), str)
 
 
-def test_for_invalid_month_values():
-    for value in [0, 13, 'outro', '*']:
-        assert get_month_name(value) == 'todos os meses'
+class TestLargeNumFormatter:
+    def test_for_formatting_of_number_under_a_thousand(self):
+        # FIXME: Remove whitespaces
+        assert large_num_formatter(1) == '1.0 '
+        assert large_num_formatter(42) == '42.0 '
+        assert large_num_formatter(999) == '999.0 '
 
+    def test_for_formatting_of_number_under_a_million(self):
+        assert large_num_formatter(1000) == '1.0 mil'
+        assert large_num_formatter(42000) == '42.0 mil'
+        assert large_num_formatter(999949) == '999.9 mil'
+        # FIXME: Format '1000.0 mil' as '1.0 Mi.'
+        assert large_num_formatter(999950) == '1000.0 mil'
 
-def test_for_return_type():
-    for month in range(1, 13):
-        assert isinstance(get_month_name(month), str)
+    def test_for_formatting_of_number_under_a_billion(self):
+        assert large_num_formatter(1000000) == '1.0 Mi.'
+        assert large_num_formatter(4200420) == '4.2 Mi.'
+        assert large_num_formatter(999949999) == '999.9 Mi.'
+        # FIXME: Format '1000.0 Mi.' as '1.0 Bi.'
+        assert large_num_formatter(999950000) == '1000.0 Mi.'
 
+    def test_for_formatting_of_number_under_a_trillion(self):
+        assert large_num_formatter(1000000000) == '1.0 Bi.'
+        assert large_num_formatter(420420420420) == '420.4 Bi.'
+        assert large_num_formatter(999949999999) == '999.9 Bi.'
+        # FIXME: Format '1000.0 Bi.' as '1.0 Tri.'
+        assert large_num_formatter(999950000000) == '1000.0 Bi.'
 
-def test_for_formatting_of_number_under_a_thousand():
-    # FIXME: Remove whitespaces
-    assert large_num_formatter(1) == '1.0 '
-    assert large_num_formatter(42) == '42.0 '
-    assert large_num_formatter(999) == '999.0 '
+    def test_for_formatting_of_number_over_a_trillion(self):
+        assert large_num_formatter(1000000000000) == '1.0 Tri.'
+        assert large_num_formatter(420420420420000) == '420.4 Tri.'
+        assert large_num_formatter(999949999999999) == '999.9 Tri.'
+        assert large_num_formatter(999950000000000) == '1000.0 Tri.'
 
-
-def test_for_formatting_of_number_under_a_million():
-    assert large_num_formatter(1000) == '1.0 mil'
-    assert large_num_formatter(42000) == '42.0 mil'
-    assert large_num_formatter(999949) == '999.9 mil'
-    # FIXME: Format '1000.0 mil' as '1.0 Mi.'
-    assert large_num_formatter(999950) == '1000.0 mil'
-
-
-def test_for_formatting_of_number_under_a_billion():
-    assert large_num_formatter(1000000) == '1.0 Mi.'
-    assert large_num_formatter(4200420) == '4.2 Mi.'
-    assert large_num_formatter(999949999) == '999.9 Mi.'
-    # FIXME: Format '1000.0 Mi.' as '1.0 Bi.'
-    assert large_num_formatter(999950000) == '1000.0 Mi.'
-
-
-def test_for_formatting_of_number_under_a_trillion():
-    assert large_num_formatter(1000000000) == '1.0 Bi.'
-    assert large_num_formatter(420420420420) == '420.4 Bi.'
-    assert large_num_formatter(999949999999) == '999.9 Bi.'
-    # FIXME: Format '1000.0 Bi.' as '1.0 Tri.'
-    assert large_num_formatter(999950000000) == '1000.0 Bi.'
-
-
-def test_for_formatting_of_number_over_a_trillion():
-    assert large_num_formatter(1000000000000) == '1.0 Tri.'
-    assert large_num_formatter(420420420420000) == '420.4 Tri.'
-    assert large_num_formatter(999949999999999) == '999.9 Tri.'
-    assert large_num_formatter(999950000000000) == '1000.0 Tri.'
--- dashboard/tests/test_data.py
+++ dashboard/tests/test_data.py
@@ -2,39 +2,37 @@ import pytest
 from src.commands.data import get_products, get_states
 
 
-def test_for_product_columns():
-    fake_csv = 'dashboard/tests/NCM-sample-iso-8859-1.csv'
-    products = get_products(fake_csv)
-    assert set(products.columns) == {'product_code', 'product'}
+class TestGetProducts:
+
+    csv_path = 'dashboard/tests/NCM-sample-iso-8859-1.csv'
+
+    def test_for_product_columns(self):
+        products = get_products(self.csv_path)
+        assert set(products.columns) == {'product_code', 'product'}
+
+    def test_for_number_of_products(self):
+        products = get_products(self.csv_path)
+        assert len(products) == 10
+
+    def test_for_specific_product(self):
+        products = get_products(self.csv_path)
+        assert 27111100 in products['product_code'].values
+        assert 'Gás natural liquefeito' in products['product'].values
 
 
-def test_for_number_of_products():
-    fake_csv = 'dashboard/tests/NCM-sample-iso-8859-1.csv'
-    products = get_products(fake_csv)
-    assert len(products) == 10
+class TestGetStates:
 
+    csv_path = 'dashboard/tests/UF-sample-iso-8859-1.csv'
 
-def test_for_specific_product():
-    fake_csv = 'dashboard/tests/NCM-sample-iso-8859-1.csv'
-    products = get_products(fake_csv)
-    assert 27111100 in products['product_code'].values
-    assert 'Gás natural liquefeito' in products['product'].values
+    def test_for_state_columns(self):
+        states = get_states(self.csv_path)
+        assert set(states.columns) == {'state', 'state_code'}
 
+    def test_for_number_of_states(self):
+        states = get_states(self.csv_path)
+        assert len(states) == 6
 
-def test_for_state_columns():
-    fake_csv = 'dashboard/tests/UF-sample-iso-8859-1.csv'
-    states = get_states(fake_csv)
-    assert set(states.columns) == {'state', 'state_code'}
-
-
-def test_for_number_of_states():
-    fake_csv = 'dashboard/tests/UF-sample-iso-8859-1.csv'
-    states = get_states(fake_csv)
-    assert len(states) == 6
-
-
-def test_for_specific_state():
-    fake_csv = 'dashboard/tests/UF-sample-iso-8859-1.csv'
-    states = get_states(fake_csv)
-    assert 'SP' in states['state_code'].values
-    assert 'São Paulo' in states['state'].values
+    def test_for_specific_state(self):
+        states = get_states(self.csv_path)
+        assert 'SP' in states['state_code'].values
+        assert 'São Paulo' in states['state'].values
