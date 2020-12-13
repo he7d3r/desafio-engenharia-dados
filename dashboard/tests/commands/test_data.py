f2774722abb5597535364140972afdd426350f4a
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2020-12-13 12:04:36 -0300

    Increase diversity of samples used by tests (and update them)

--- dashboard/tests/commands/test_data.py
+++ dashboard/tests/commands/test_data.py
@@ -7,16 +7,23 @@ class TestGetProducts:
 
     def test_for_product_columns(self):
         products = get_products(self.csv_path)
-        assert set(products.columns) == {'product_code', 'product'}
+        actual = set(products.columns)
+        expected = {'product_code', 'product'}
+        assert actual == expected
 
-    def test_for_number_of_products(self):
+    def test_for_product_codes(self):
         products = get_products(self.csv_path)
-        assert len(products) == 10
+        actual = list(products['product_code'].values)
+        expected = [22086000, 19059020, 29393010, 29061100, 48202000,
+                    84831040, 96091000, 90153000, 90051000]
+        assert actual == expected
 
-    def test_for_specific_product(self):
+    def test_for_product_names(self):
         products = get_products(self.csv_path)
-        assert 27111100 in products['product_code'].values
-        assert 'Gás natural liquefeito' in products['product'].values
+        actual = list(products['product'].values)
+        expected = ['Vodca', 'Bolachas', 'Cafeína', 'Mentol', 'Cadernos',
+                    'Manivelas', 'Lápis', 'Níveis', 'Binóculos']
+        assert actual == expected
 
 
 class TestGetStates:
@@ -25,16 +32,22 @@ class TestGetStates:
 
     def test_for_state_columns(self):
         states = get_states(self.csv_path)
-        assert set(states.columns) == {'state', 'state_code'}
+        actual = set(states.columns)
+        expected = {'state', 'state_code'}
+        assert actual == expected
 
-    def test_for_number_of_states(self):
+    def test_for_state_codes(self):
         states = get_states(self.csv_path)
-        assert len(states) == 6
+        actual = list(states['state_code'].values)
+        expected = ['TO', 'RJ', 'SC', 'GO', 'ZN']
+        assert actual == expected
 
-    def test_for_specific_state(self):
+    def test_for_state_names(self):
         states = get_states(self.csv_path)
-        assert 'SP' in states['state_code'].values
-        assert 'São Paulo' in states['state'].values
+        actual = list(states['state'].values)
+        expected = ['Tocantins', 'Rio de Janeiro', 'Santa Catarina', 'Goiás',
+                    'Zona Não Declarada']
+        assert actual == expected
 
 
 class TestAggregateByStateAndAdd:
@@ -43,7 +56,7 @@ class TestAggregateByStateAndAdd:
     products_path = 'dashboard/tests/NCM-sample-iso-8859-1.csv'
     states_path = 'dashboard/tests/UF-sample-iso-8859-1.csv'
 
-    def test_single_imported_product(self, db, runner):
+    def test_top_product_by_state(self, db, runner):
         result = runner.invoke(args=[
             'data',
             'aggregate-by-state-and-add',
@@ -61,7 +74,11 @@ class TestAggregateByStateAndAdd:
         assert expected in result.output
 
         query = 'SELECT * FROM top_by_state_and_year'
-        expected = [('PR', 'Paraná', 2019, 'import', 19019020, 'Doce de leite',
-                    267286)]
+        expected = [
+            ('GO', 'Goiás', 2019, 'import', 29061100, 'Mentol', 6126),
+            ('RJ', 'Rio de Janeiro', 2019, 'import', 22086000, 'Vodca', 32096),
+            ('SC', 'Santa Catarina', 2019, 'import', 96091000, 'Lápis',
+             1306958),
+            ('TO', 'Tocantins', 2019, 'import', 90051000, 'Binóculos', 8099)]
         actual = db.engine.execute(query).fetchall()
-        assert expected == actual
+        assert actual == expected
--- dashboard/tests/commands/test_database.py
+++ dashboard/tests/commands/test_database.py
@@ -4,44 +4,54 @@ from src.database import db
 class TestCreate:
 
     def test_tables_created(self, runner):
+        # Before running the command
+        actual = db.engine.table_names()
         expected = []
-        assert expected == db.engine.table_names()
+        assert actual == expected
 
+        # After running the command
         result = runner.invoke(args=['database', 'create'])
-
+        actual = db.engine.table_names()
         expected = [
             'state_contributions',
             'top_by_state_and_month',
             'top_by_state_and_year'
         ]
-        assert expected == db.engine.table_names()
+        assert actual == expected
         assert 'All tables were created' in result.output
 
 
 class TestDrop:
 
     def test_confirm(self, db, runner):
+        # Before running the command
+        actual = db.engine.table_names()
         expected = [
             'state_contributions',
             'top_by_state_and_month',
             'top_by_state_and_year'
         ]
-        assert expected == db.engine.table_names()
+        assert actual == expected
 
+        # After running the command
         result = runner.invoke(args=['database', 'drop'], input='y')
-
+        actual = db.engine.table_names()
         expected = []
-        assert expected == db.engine.table_names()
+        assert actual == expected
         assert 'All tables were dropped' in result.output
 
     def test_abort(self, db, runner):
-
+        # Before running the command
+        actual = db.engine.table_names()
         expected = [
             'state_contributions',
             'top_by_state_and_month',
             'top_by_state_and_year'
         ]
-        assert expected == db.engine.table_names()
+        assert actual == expected
+
+        # After running the command
         result = runner.invoke(args=['database', 'drop'], input='N')
+        actual = db.engine.table_names()
         assert 'Aborted!' in result.output
-        assert expected == db.engine.table_names()
+        assert actual == expected
